6e9be691433923c3fdf0005beefa8ac663759db8
Rustam Safin <rustiksnegovik@gmail.com>
2025-06-16 14:14:56 +0300

    Initial commit

---
+++ Casks/Development.rb
@@ -0,0 +1,18 @@
+cask "Development cTrader" do
+  version :latest
+  sha256 :no_check
+
+  url "https://getctrader.lim.spotwa.re/mac/devbuild/ctrader-devbuild-setup.dmg"
+  name "Development cTrader"
+  desc "Development"
+  homepage "https://ctrader.com/"
+
+  app "Development cTrader.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-d", "com.apple.quarantine", "#{appdir}/Development cTrader.app"],
+                   sudo: false
+  end
+end
+
